784851114660e84dfc3d7dbe009bfb4643130856
bootes16 <gjjohnson@gmail.com>
2021-01-25 18:15:04 +1100

    Add confidence interval

--- mathx/stats/basic_stats.go
+++ mathx/stats/basic_stats.go
@@ -1,51 +1,55 @@
+// Package stats implements some basic descriptive statistics functions.
 package stats
 
 import "math"
 
-// Accumulated statistics
-// sum of values
-// sum of square of values
-// number of values
-type context struct {
+// Context is the struct holding the accumulated statistics.
+type Context struct {
 	sigma   float64
 	sigmaSq float64
 	count   int
 }
 
-func New() *context {
-	return new(context)
+const ci95 float64 = 1.960
+
+// New creates and returns a new stats context.
+func New() *Context {
+	return new(Context)
 }
 
-func (ctx *context) Reset() {
+// Reset clears/resets the stats context.
+func (ctx *Context) Reset() {
 	ctx.sigma = 0.0
 	ctx.sigmaSq = 0.0
 	ctx.count = 0
 }
 
-func (ctx *context) GetCount() int {
+// Count returns a count of the number of values accumulated.
+func (ctx *Context) Count() int {
 	return ctx.count
 }
 
-func (ctx *context) Add(vals ...float64) {
+func (ctx *Context) Add(vals ...float64) {
 	for _, v := range vals {
 		ctx.sigma += v
-		ctx.sigmaSq += v * v
+		// ctx.sigmaSq += v * v
+		ctx.sigmaSq = math.FMA(v, v, ctx.sigmaSq)
 		ctx.count++
 	}
 }
 
-func (ctx *context) Sum() float64 {
+func (ctx *Context) Sum() float64 {
 	return ctx.sigma
 }
 
-func (ctx *context) Mean() float64 {
+func (ctx *Context) Mean() float64 {
 	if 0 == ctx.count {
 		return 0.0
 	}
 	return ctx.sigma / float64(ctx.count)
 }
 
-func (ctx *context) Stdev(ddof int) float64 {
+func (ctx *Context) Stdev(ddof int) float64 {
 	if ctx.count < 2 {
 		return 0.0
 	}
@@ -55,10 +59,17 @@ func (ctx *context) Stdev(ddof int) float64 {
 	return math.Sqrt(variance)
 }
 
-func (ctx *context) StdevS() float64 {
+func (ctx *Context) StdevS() float64 {
 	return ctx.Stdev(1)
 }
 
-func (ctx *context) StdevP() float64 {
+func (ctx *Context) StdevP() float64 {
 	return ctx.Stdev(0)
+}
+
+func (ctx *Context) Ci95() float64 {
+	if ctx.count < 2 {
+		return 0.0
+	}
+	return ci95 * (ctx.StdevS() / math.Sqrt(float64(ctx.count)))
 }
--- test/basic_test.go
+++ test/basic_test.go
@@ -1,10 +1,15 @@
 package main
 
 import (
-	"main/mathx/stats"
 	"math"
 	"testing"
+
+	"../mathx/stats"
 )
+
+func compareFloat(a float64, b float64, tolerance float64) bool {
+	return math.Abs(a-b) <= tolerance
+}
 
 func TestStatsCreate(t *testing.T) {
 	s := stats.New()
@@ -47,5 +52,14 @@ func TestStatsStdDev(t *testing.T) {
 	delta := math.Abs(popStdev - 1.633)
 	if delta > 0.0005 {
 		t.Errorf("expected: %f received: %f", 1.633, popStdev)
+	}
+}
+
+func TestStatsCI(t *testing.T) {
+	s := stats.New()
+	s.Add(25.0, 35.0, 10.0, 17.0, 29.0, 14.0, 21.0, 31.0)
+	ci := s.Ci95()
+	if !compareFloat(ci, 6.0723, 0.0001) {
+		t.Errorf("Confidence interval is %f", ci)
 	}
 }
